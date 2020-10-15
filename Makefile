1d803f146de9316b12c3ae8e75c8ededbf3b029c
MATT JOHNSON <johnson.matt1818@gmail.com>
2020-10-14 20:44:14 -0400

    Added more tidying

--- Makefile
+++ Makefile
@@ -3,8 +3,18 @@
 clean:
 				rm derived_data/*
 				
-				
+
+derived_data/OLine.csv\
+derived_data/QB.csv\
+derived_data/TE.csv\
+derived_data/RB.csv\
+derived_data/WR.csv\
+derived_data/FB.csv\
+derived_data/LB.csv\
+derived_data/DLine.csv\
+derived_data/DB.csv\
 derived_data/Defense.csv\
+derived_data/Full.DF.csv\
 derived_data/Offense.csv: Source-Data/Final.csv\
  tidy_data.R
 	Rscript tidy_data.R
--- tidy_data.R
+++ tidy_data.R
@@ -7,10 +7,68 @@ DF <- read.csv("Source-Data/Final.csv") %>%
 
 
 DF.Def <- DF %>% 
-  filter(pos == "DE" | pos == "LB" | pos == "DT"| pos == "DB"| pos == "NT"| pos == "DL"| pos == "OLB"| pos == "CB"| pos == "S"| pos == "ILB"| pos == "FS"| pos == "SS" | pos == "")
+  filter(pos == "DE" | pos == "LB" | pos == "DT"| pos == "DB"| pos == "NT"| pos == "DL"| pos == "OLB"| pos == "CB"| pos == "S"| pos == "ILB"| pos == "FS"| pos == "SS")
 
 DF.Off <- DF %>% 
   filter(pos == "T" | pos == "WR" | pos == "RB" | pos == "TE" | pos == "QB" | pos == "G" | pos == "C" | pos == "FB" | pos == "OL" | pos == "OG" | pos == "OT")
 
 write.csv(DF.Def, "derived_data/Defense.csv")
 write.csv(DF.Off, "derived_data/Offense.csv")
+
+OLine.DF <- DF.Off %>% 
+  filter(pos == "T" | pos == "G"| pos == "C"| pos == "OL"| pos == "OG"| pos == "OT") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+WR.DF <- DF.Off %>% 
+  filter(pos == "WR") %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical) %>% 
+  filter(carav > 0)
+
+QB.DF <- DF.Off %>% 
+  filter(pos == "QB") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+RB.DF <- DF.Off %>% 
+  filter(pos == "RB") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+TE.DF <- DF.Off %>% 
+  filter(pos == "TE") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+FB.DF <- DF.Off %>% 
+  filter(pos == "FB") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+LB.DF <- DF.Def %>% 
+  filter(pos == "LB" | pos == "OLB" | pos == "ILB") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+DLine.DF <- DF.Def %>% 
+  filter(pos == "NT"| pos == "DL" | pos == "DE" | pos == "DT") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+DB.DF <- DF.Def %>% 
+  filter(pos == "DB" | pos == "CB"| pos == "S"| pos == "FS"| pos == "SS") %>% 
+  filter(carav > 0) %>% 
+  select(player, pick, pos, year, age, carav, bench, broad, forty, threecone, shuttle, vertical)
+
+Full.DF <- rbind(DB.DF, DLine.DF, LB.DF, FB.DF, TE.DF, RB.DF, WR.DF, QB.DF, OLine.DF)
+
+write.csv(OLine.DF, "derived_data/OLine.csv")
+write.csv(QB.DF, "derived_data/QB.csv")
+write.csv(TE.DF, "derived_data/TE.csv")
+write.csv(RB.DF, "derived_data/RB.csv")
+write.csv(WR.DF, "derived_data/WR.csv")
+write.csv(FB.DF, "derived_data/FB.csv")
+write.csv(LB.DF, "derived_data/LB.csv")
+write.csv(DLine.DF, "derived_data/DLine.csv")
+write.csv(DB.DF, "derived_data/DB.csv")
+write.csv(Full.DF, "derived_data/Full.DF.csv")
